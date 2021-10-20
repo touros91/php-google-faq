9e61b98aa434ef9bb46b14722c9a24e6111d0688
touros91 <andreageraci91@gmail.com>
2021-10-20 16:42:49 +0200

    Refactoring

--- index.php
+++ index.php
@@ -66,13 +66,15 @@ $faq = [
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/style.css">
+    <!-- Google Favicon -->
+    <link rel="shortcut icon" href="https://upload.wikimedia.org/wikipedia/commons/2/2d/Google-favicon-2015.png" type="image/x-icon">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- Google Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <title>Google Faq</title>
+    <title>Domande frequenti</title>
 </head>
 <body>
     <!-- Header  -->
@@ -85,7 +87,9 @@ $faq = [
                 <h1>Privacy e termini</h1>
             </div>
             <div class="app-accedi">
-                <i class="fas fa-braille"></i>
+                <div class="icon-box">
+                    <i class="fas fa-grip-vertical"></i>
+                </div>
                 <button>Accedi</button>
             </div>
         </section>
