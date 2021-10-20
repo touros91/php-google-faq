db4e7b3ff7c9f3cf369ec73fb88ba138596eb09e
touros91 <andreageraci91@gmail.com>
2021-10-20 14:47:22 +0200

    Aggiunto header completo di stile

--- index.php
+++ index.php
@@ -57,14 +57,40 @@ $faq = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <title>Google Faq</title>
 </head>
 <body>
     <header>
-        <div class="logo">
-            <img src="img/googlelogo_clr_74x24px.svg" alt="Google Logo">
-        </div>
+        <section class="header-top">
+            <div class="logo-title">
+                <div class="logo">
+                    <img src="img/googlelogo_clr_74x24px.svg" alt="Google Logo">
+                </div>
+                <h1>Privacy e termini</h1>
+            </div>
+            <div class="app-accedi">
+                <i class="fas fa-braille"></i>
+                <button>Accedi</button>
+            </div>
+        </section>
+        <section class="header-bottom">
+            <ul>
+                <li>Introduzione</li>
+                <li>Norme sulla privacy</li>
+                <li>Termini di servizio</li>
+                <li>Tecnologie</li>
+                <li>Domande frequenti</li>
+            </ul>
+        </section>
     </header>
+
     <main>
         <div>
             <?php foreach($faq as $domande) { ?>
